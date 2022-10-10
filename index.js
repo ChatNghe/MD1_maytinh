dcdafc04d4542dbfa49989a0c47b1d86230f45e0
ChatNghe <namnt98@gmail.com>
2022-10-10 12:00:25 +0700

    thaydoi2

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ function add(){
     let a = +document.getElementById("a").value;
     let b = +document.getElementById("b").value;
     let Result;
-    Result = "Result = "+ (a + b) ;
+    Result = "Result Addition = "+ (a + b) ;
     document.getElementById("Result").innerHTML = Result;
 }
 function sub(){
     let a = +document.getElementById("a").value;
     let b = +document.getElementById("b").value;
     let Result;
-    Result = "Result = "+ (a - b) ;
+    Result = "Result Subtraction = "+ (a - b) ;
     document.getElementById("Result").innerHTML = Result;
 }
 function mul(){
     let a = +document.getElementById("a").value;
     let b = +document.getElementById("b").value;
     let Result;
-    Result = "Result = "+ (a * b) ;
+    Result = "Result Multiplication = "+ (a * b) ;
     document.getElementById("Result").innerHTML = Result;
 }
 function div(){
     let a = +document.getElementById("a").value;
     let b = +document.getElementById("b").value;
     let Result;
-    Result = "Result = "+ (a / b) ;
+    Result = "Result Division = "+ (a / b) ;
     document.getElementById("Result").innerHTML = Result;
 }
